b4993047dcfddc72fb0b4bfc4bc3578cf204393b
Kris Lee <kdlee0401@gmail.com>
2024-07-23 17:44:14 -0500

    updated block quote

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 '''
-We are using packages: 'openai', 'python-dotenv'.
+We are using modules: 'openai', 'python-dotenv'.
 make sure these versions >= 'python-dotenv 0.21.0', 'openai 1.0.0'.
 
 When using this file. PLEASE make sure to use your own API key from OpenAI and place it in a .env file
-with the name 'API_KEY'. Otherwise, you will need to make manually adjustments to match your named variables.
+with the name 'API_KEY'. Otherwise, you will need to make adjustments to match your named variables.
 '''
 
 import openai
